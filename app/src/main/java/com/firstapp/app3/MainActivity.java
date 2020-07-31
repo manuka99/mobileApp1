c7f25b9bf5b940f730078217ee84677f97dd11c1
manukayasas94@gmail.com <manukad1>
2020-08-01 01:18:06 +0530

    add onclick 2

--- app/src/main/java/com/firstapp/app3/MainActivity.java
+++ app/src/main/java/com/firstapp/app3/MainActivity.java
@@ -5,9 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView text;
 
     public static final String Extra_Message = "com.firstapp.app3";
 
@@ -15,6 +20,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.onThis);
+        text = findViewById(R.id.editText);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                text.setText("Hey all");
+
+            }
+        });
+
     }
 
     public void sendMessage(View view){
